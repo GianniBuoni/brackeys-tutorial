c068c574eef8aba2edde10d130f67e74b8efea62
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-08-18 15:55:27 -0400

    init commit: boilerplate godot-rust project

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = ''
+    Starter Ratatui dev and build flake.
+  '';
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {inherit system;};
+        nativeBuildInputs = with pkgs; [
+          cargo
+          clippy
+          just
+          rustc
+          rust-analyzer
+          pkg-config
+        ];
+        buildInputs = [
+        ];
+        naersk' = pkgs.callPackage naersk {};
+        mkApp = release: mode: {
+          src = ./rust/.;
+          copyLibs = true;
+          inherit nativeBuildInputs buildInputs release mode;
+        };
+      in {
+        packages = {
+          default = naersk'.buildPackage (mkApp true "build");
+          debug = naersk'.buildPackage (mkApp false "build");
+          test = naersk'.buildPackage (mkApp false "test");
+        };
+        devShell = pkgs.mkShell {
+          inherit nativeBuildInputs buildInputs;
+        };
+      }
+    );
+}
