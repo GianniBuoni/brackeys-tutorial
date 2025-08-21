7dc7839584fa0010f413576eba1ed79eac71587d
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-08-20 22:13:50 -0400

    feat: remap inputs, start using Godot actions
    - add sprite flipping to player
    - add jump/run animations
    - refactor: split up PlaylerController::physics_process() into mutliple
      different setters

--- rust/src/player_controller.rs
+++ rust/src/player_controller.rs
@@ -1,5 +1,5 @@
 use godot::{
-    classes::{CharacterBody2D, ICharacterBody2D, Input},
+    classes::{AnimatedSprite2D, CharacterBody2D, ICharacterBody2D, Input},
     global::move_toward,
     prelude::*,
 };
@@ -9,27 +9,63 @@ use crate::prelude::*;
 #[derive(GodotClass)]
 #[class(init, base=CharacterBody2D)]
 pub struct PlayerController {
+    #[init(node = "AnimatedSprite2D")]
+    sprite: OnReady<Gd<AnimatedSprite2D>>,
     base: Base<CharacterBody2D>,
 }
 
 #[godot_api]
 impl ICharacterBody2D for PlayerController {
     fn physics_process(&mut self, delta: f32) {
-        let mut base = self.base_mut();
-        let mut velocity = base.get_velocity();
+        let base = self.base();
+        let mut velocity = self.base().get_velocity();
 
         // add gravity
         if !base.is_on_floor() {
             velocity += base.get_gravity() * delta;
         }
         // handle jump
-        if Input::singleton().is_action_just_pressed("ui_accept")
+        if Input::singleton().is_action_just_pressed("jump")
             && base.is_on_floor()
         {
             velocity.y = PLAYER_JUMP;
         }
-        // handle input
-        let direction = Input::singleton().get_axis("ui_left", "ui_right");
+        // direction -1, 0, 1
+        let direction = Input::singleton().get_axis("move_left", "move_right");
+
+        self.set_flip(direction);
+        self.set_animation(direction);
+        self.set_velocity(direction, &mut velocity);
+    }
+}
+
+impl PlayerController {
+    fn set_animation(&mut self, direction: f32) {
+        if !self.base().is_on_floor() {
+            self.sprite.set_animation("jump");
+            return;
+        }
+        match direction {
+            -1. | 1. => {
+                self.sprite.set_animation("run");
+            }
+            _ => {
+                self.sprite.set_animation("idle");
+            }
+        }
+    }
+    fn set_flip(&mut self, direction: f32) {
+        match direction {
+            -1. => {
+                self.sprite.set_flip_h(true);
+            }
+            1. => {
+                self.sprite.set_flip_h(false);
+            }
+            _ => {}
+        }
+    }
+    fn set_velocity(&mut self, direction: f32, velocity: &mut Vector2) {
         match direction {
             dir if dir != 0. => velocity.x = direction * PLAYER_SPEED,
             _ => {
@@ -38,7 +74,8 @@ impl ICharacterBody2D for PlayerController {
                         as f32
             }
         }
-        base.set_velocity(velocity);
+        let mut base = self.base_mut();
+        base.set_velocity(*velocity);
         base.move_and_slide();
     }
 }
